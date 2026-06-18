cf9295fc252d4cb3c44f5b400f2bf15d03c6c652
Thanseer Hishak <96327516+Sp00fyBoi@users.noreply.github.com>
2026-06-18 14:08:32 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+            map = {}
+            if len(s) == len(t) :
+                for i in s:
+                    if i in map:
+                        map[i] = map[i] + 1
+                    else:
+                        map[i] = 1
+                for i in t:
+                    if i in map:
+                        if map[i] == 1:
+                            del map[i]
+                        else:
+                            map[i] = map[i] - 1
+                return len(map) == 0
+            return False
---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for index, value in enumerate(nums):
+            if target - value in map:
+                return [map[target - value], index]
+            map[value] = index
